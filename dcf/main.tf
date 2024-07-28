ab18b2f1fedd7a3d284ab058e473c1f76763b13d
Ricardo Trentin <rtrentin@aviatrix.com>
2024-07-28 13:15:25 -0400

    greenfield rule

--- dcf/main.tf
+++ dcf/main.tf
@@ -15,24 +15,50 @@ resource "aviatrix_smart_group" "smarties" {
   }
 }
 
-resource "aviatrix_distributed_firewalling_policy_list" "test" {
+# resource "aviatrix_distributed_firewalling_policy_list" "policies" {
+#   dynamic "policies" {
+#     for_each = var.policies
+#     content {
+#       name                     = policies.key
+#       action                   = policies.value.action
+#       priority                 = policies.value.priority
+#       protocol                 = policies.value.protocol
+#       logging                  = policies.value.logging
+#       watch                    = policies.value.watch
+#       src_smart_groups         = [local.smart_groups_map[policies.value.src_smart_groups]]
+#       dst_smart_groups         = [local.smart_groups_map[policies.value.dst_smart_groups]]
+#       flow_app_requirement     = "APP_UNSPECIFIED"
+#       decrypt_policy           = "DECRYPT_UNSPECIFIED"
+#       exclude_sg_orchestration = false
+#       port_ranges {
+#         hi = policies.key == "Greenfield-Rule" ? Null : policies.value.port_range_high
+#         lo = policies.key == "Greenfield-Rule" ? Null : policies.value.port_range_low
+#       }
+#     }
+#   }
+# }
+resource "aviatrix_distributed_firewalling_policy_list" "policies" {
   dynamic "policies" {
     for_each = var.policies
     content {
-      name             = policies.key
-      action           = policies.value.action
-      priority         = policies.value.priority
-      protocol         = policies.value.protocol
-      logging          = policies.value.logging
-      watch            = policies.value.watch
-      src_smart_groups = [local.smart_groups_map[policies.value.src_smart_groups]]
-      dst_smart_groups = [local.smart_groups_map[policies.value.dst_smart_groups]]
-      flow_app_requirement = "APP_UNSPECIFIED"
-      decrypt_policy = "DECRYPT_UNSPECIFIED"
+      name                     = policies.key
+      action                   = policies.value.action
+      priority                 = policies.value.priority
+      protocol                 = policies.value.protocol
+      logging                  = policies.value.logging
+      watch                    = policies.value.watch
+      src_smart_groups         = [local.smart_groups_map[policies.value.src_smart_groups]]
+      dst_smart_groups         = [local.smart_groups_map[policies.value.dst_smart_groups]]
+      flow_app_requirement     = "APP_UNSPECIFIED"
+      decrypt_policy           = "DECRYPT_UNSPECIFIED"
       exclude_sg_orchestration = false
-      port_ranges {
-        hi = policies.value.port_range_high
-        lo = policies.value.port_range_low
+
+      dynamic "port_ranges" {
+        for_each = can(policies.value.port_range_high) && can(policies.value.port_range_low) ? [1] : []
+        content {
+          hi = policies.value.port_range_high
+          lo = policies.value.port_range_low
+        }
       }
     }
   }
